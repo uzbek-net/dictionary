8eee5a8f18726d4fd75f20b06800119afb7d7f96
bahrom04 <magdiyevbahrom@gmail.com>
2026-03-17 16:52:46 +0500

    refactor: created pip package structure

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+import apostrophe_normalize_list
+import apostrophe_normalizer
+import fuzzy_remover
+import get_gnome_po_translations
+import toml2po_converter
+
+__all__ = [
+    "apostrophe_normalize_list",
+    "apostrophe_normalizer",
+    "fuzzy_remover",
+    "get_gnome_po_translations",
+    "toml2po_converter",
+]
---
+++ src/__main__.py
@@ -0,0 +1,8 @@
+import apostrophe_normalize_list
+import apostrophe_normalizer
+import fuzzy_remover
+import get_gnome_po_translations
+import toml2po_converter
+
+if __name__ == "__main__":
+    print("this is test")
